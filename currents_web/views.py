d9b2de1702f94011e4980c6ab1d6aaf290e6866c
gandalf-the-lonesome <abin.xv@gmail.com>
2023-02-11 16:09:46 +0530

    added home.html

--- currents_web/views.py
+++ currents_web/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "currents_web/base.html")
+
+def home(request):
+    return render(request, "currents_web/home.html")
