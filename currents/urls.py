bb211e60f917dfc1a520844dff687dfe0cdb2490
gandalf-the-lonesome <abin.xv@gmail.com>
2023-02-11 13:01:50 +0530

    fix

--- currents/urls.py
+++ currents/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('currents_web/', include('currents_web.urls')),    
+    path('', include('currents_web.urls')),    
     path('admin/', admin.site.urls),
 ]
--- currents_web/views.py
+++ currents_web/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "currents_web/base.html")
+    return render(request, "base.html")
