ce1a7cba464c26f6ef8b626b8ed78d14a5539403
RizinGH <rizi.n2021@gmail.com>
2023-03-28 14:51:49 +0530

    updates

--- currents_web/urls.py
+++ currents_web/urls.py
@@ -6,13 +6,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('fy', views.fy, name='fy'),
-    path('profile', views.profile, name='profile'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('weather', views.weather, name='weather'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('subscribe',views.subscribe,name='subscribe'),
-    path('about',views.about,name='about'),
+    path('fy/', views.fy, name='fy'),
+    path('profile/', views.profile, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('weather/', views.weather, name='weather'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('subscribe/',views.subscribe,name='subscribe'),
+    path('about/',views.about,name='about'),
 
 ]
